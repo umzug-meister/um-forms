918ae01d9cc5173f4b8722025a29638368bdc47a
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-04-26 17:32:31 +0200

    fixed urls

--- src/components/OfferCard.tsx
+++ src/components/OfferCard.tsx
@@ -26,7 +26,7 @@ export function OfferCard({ transporter, workers, primary, secondary }: Props) {
   const navigate = useNavigate();
   const selectedPriceID = useSelectedId();
   const imageSrc = useMemo(
-    () => `${import.meta.env.BASE_URL}/${workers}_${transporter}.png`,
+    () => `${import.meta.env.VITE_RESOURCE_URL}/${workers}_${transporter}.png`,
     [transporter, workers]
   );
 
