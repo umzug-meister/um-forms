eca8a3d8d7b12be04bf7f1561a174581b1ffa96f
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-04-26 17:37:21 +0200

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/um-forms/express/",
   plugins: [react()],
   build: {
     rollupOptions: {
