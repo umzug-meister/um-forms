fa81750c717baf3f82552fea11346106be3cc1be
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-11-20 19:37:50 +0100

    update public path

--- vite.full.config.ts
+++ vite.full.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === "development" ? "/" : "/um-forms/full/",
+    base: mode === "development" ? "/" : "/um-forms/form/",
     build: {
       rollupOptions: {
         input: {
