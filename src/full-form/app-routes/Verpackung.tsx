bd9562967e679ce98c15aaaddaebf07fbb9aa464
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-11-22 18:57:20 +0100

    render packings

--- src/full-form/app-routes/Verpackung.tsx
+++ src/full-form/app-routes/Verpackung.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useSelector } from "react-redux";
+import { AppPacking, Service } from "um-types";
 import ContainerBox from "../../shared/components/ContainerBox";
+import { AppState } from "../../store";
 
 export default function Verpackung() {
+  const services = useSelector<AppState, Service[]>((s) => s.app.services);
+
+  const packings = services.filter(
+    (s) => s.tag === "Packmaterial"
+  ) as AppPacking[];
+
   return (
     <ContainerBox title="Verpackung benötigt?">
-      Verpackung sollte hier stehen ist aber defekt...
+      {packings.map((packing) => (
+        <PackingCard key={packing.id} packing={packing} />
+      ))}
     </ContainerBox>
+  );
+}
+interface Props {
+  packing: AppPacking;
+}
+
+function PackingCard({ packing }: Readonly<Props>) {
+  const formatter = new Intl.NumberFormat("de-DE", {
+    currency: "EUR",
+    style: "currency",
+  });
+  return (
+    <Card variant="outlined">
+      {packing.media && <CardMedia image={packing.media} />}
+      <CardMedia></CardMedia>
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {packing.name}
+        </Typography>
+        <Typography variant="body2">{packing.desc}</Typography>
+        <Typography gutterBottom variant="h5">
+          {formatter.format(Number(packing.price))}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
