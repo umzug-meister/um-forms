db294c83d23104d0c50eacc6ac7696fdef1e23f5
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-11-22 19:27:09 +0100

    render verpackung

--- src/full-form/app-routes/Verpackung.tsx
+++ src/full-form/app-routes/Verpackung.tsx
@@ -3,6 +3,7 @@ import {
   CardContent,
   CardHeader,
   CardMedia,
+  Grid,
   Typography,
 } from "@mui/material";
 import { useSelector } from "react-redux";
@@ -27,11 +28,11 @@ export default function Verpackung() {
         label="Möchten Sie Verpackung bei uns kaufen?"
       />
       {order.needPackings && (
-        <>
+        <Grid container spacing={4}>
           {packings.map((packing) => (
             <PackingCard key={packing.id} packing={packing} />
           ))}
-        </>
+        </Grid>
       )}
     </ContainerBox>
   );
@@ -47,18 +48,20 @@ function PackingCard({ packing }: Readonly<Props>) {
   });
 
   return (
-    <Card variant="outlined">
-      {packing.media && <CardMedia image={packing.media} />}
-      <CardMedia></CardMedia>
-      <CardContent>
-        <Typography gutterBottom variant="h5">
-          {packing.name}
-        </Typography>
-        <Typography variant="body2">{packing.desc}</Typography>
-        <Typography gutterBottom variant="h5">
-          {formatter.format(Number(packing.price))}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Grid item xs={12} sm={6}>
+      <Card sx={{ height: "100%" }}>
+        {packing.media && <CardMedia image={packing.media} />}
+        <CardMedia></CardMedia>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {packing.name}
+          </Typography>
+          <Typography variant="body2">{packing.desc}</Typography>
+          <Typography gutterBottom variant="h5">
+            {formatter.format(Number(packing.price))}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
