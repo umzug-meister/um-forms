e4aba7b4453d18b810351cc29852a27936c9b25d
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-11-28 17:54:49 +0100

    run

--- src/main.full.tsx
+++ src/main.full.tsx
@@ -10,7 +10,6 @@ const ROOT_NODE = "um-full-form-root";
 export function scrollToRoot() {
   return scrollToElement(ROOT_NODE);
 }
-
 ReactDOM.createRoot(document.getElementById(ROOT_NODE) as HTMLElement).render(
   <React.StrictMode>
     <App />
