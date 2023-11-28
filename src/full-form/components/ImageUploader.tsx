9d6b38c1a2e4b905cfdb55d9805775fd7581d46e
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-11-28 20:07:47 +0100

    Update ImageUploader.tsx

--- src/full-form/components/ImageUploader.tsx
+++ src/full-form/components/ImageUploader.tsx
@@ -6,7 +6,7 @@ import Card from "@mui/material/Card";
 import CircularProgress from "@mui/material/CircularProgress";
 import IconButton from "@mui/material/IconButton";
 
-import { CognitoIdentityCredentials, config, S3 } from "aws-sdk";
+import * as AWS from "aws-sdk";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Order, SendData } from "um-types";
@@ -17,9 +17,9 @@ import { AppState } from "../../store";
 import { addImageData, removeImageData } from "../../store/appReducer";
 
 function initAws(poolId: string) {
-  config.update({
+  AWS.config.update({
     region: "eu-central-1",
-    credentials: new CognitoIdentityCredentials({
+    credentials: new AWS.CognitoIdentityCredentials({
       IdentityPoolId: poolId,
     }),
   });
@@ -49,14 +49,14 @@ export const ImageUploader = () => {
   function onFilesChange(event: React.ChangeEvent<HTMLInputElement>) {
     const files = event.target.files;
     if (files !== null) {
-      const promises = new Array<Promise<S3.ManagedUpload.SendData>>();
+      const promises = new Array<Promise<AWS.S3.ManagedUpload.SendData>>();
       setShowLoading(true);
       const path = createFolderPath();
 
       for (let i = 0; i < files.length; i++) {
         let currentFile = files.item(i);
         if (currentFile !== null) {
-          const upload = new S3.ManagedUpload({
+          const upload = new AWS.S3.ManagedUpload({
             params: {
               Bucket: "umzug.meister",
               Key: path.concat(currentFile.name),
