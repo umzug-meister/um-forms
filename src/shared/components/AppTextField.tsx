435616838caed00f4cf175b962d9c876bc6eaf3e
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-12-01 14:59:05 +0100

    update app text field

--- src/shared/components/AppTextField.tsx
+++ src/shared/components/AppTextField.tsx
@@ -1,51 +1,32 @@
 import { TextField, TextFieldProps } from "@mui/material";
 
 export function AppTextField(props: TextFieldProps) {
+  const cssProps = (size: number) => {
+    return {
+      "& input[type=text]": {
+        border: "none !important",
+        padding: `${size}px!important`,
+      },
+      "& input[type=email]": {
+        border: "none !important",
+        padding: `${size}px!important`,
+      },
+      "& input[type=number]": {
+        border: "none !important",
+        padding: `${size}px!important`,
+        appearance: "textfield",
+        MozAppearance: "textfield",
+        WebkitAppearance: "textfield",
+      },
+      "& input[type=number]::-webkit-inner-spin-button": {
+        display: "none",
+      },
+    };
+  };
+
   return (
     <TextField
-      sx={
-        props.size === "small"
-          ? {
-              "& input[type=text]": {
-                border: "none !important",
-                padding: "10px!important",
-              },
-              "& input[type=email]": {
-                border: "none !important",
-                padding: "10px!important",
-              },
-              "& input[type=number]": {
-                border: "none !important",
-                padding: "10px!important",
-                appearance: "textfield",
-                MozAppearance: "textfield",
-                WebkitAppearance: "textfield",
-              },
-              "& input[type=number]::-webkit-inner-spin-button": {
-                display: "none",
-              },
-            }
-          : {
-              "& input[type=text]": {
-                border: "none !important",
-                padding: "16px!important",
-              },
-              "& input[type=email]": {
-                border: "none !important",
-                padding: "16px!important",
-              },
-              "& input[type=number]": {
-                border: "none !important",
-                padding: "16px!important",
-                appearance: "textfield",
-                MozAppearance: "textfield",
-                WebkitAppearance: "textfield",
-              },
-              "& input[type=number]::-webkit-inner-spin-button": {
-                display: "none",
-              },
-            }
-      }
+      sx={props.size === "small" ? cssProps(10) : cssProps(16)}
       fullWidth
       variant="outlined"
       {...props}
