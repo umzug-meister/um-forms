51623f7bdc5892924bfcca9619aba5994f1c608c
Nikolai Kabiolskij <12097812+mee3003@users.noreply.github.com>
2023-12-09 12:45:05 +0100

    align end text

--- src/full-form/components/End.tsx
+++ src/full-form/components/End.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren } from "react";
 
 export function End({ children }: PropsWithChildren) {
   return (
-    <Typography color={"primary"} variant="subtitle2">
+    <Typography color={"primary"} variant="caption">
       {children}
     </Typography>
   );
