abd2c74813921960c1057207399e588e93440279
Nikolai K. <sputnyq@gmail.com>
2026-05-27 16:20:00 +0200

    fix: button color set

--- src/shared/components/AppButton.tsx
+++ src/shared/components/AppButton.tsx
@@ -1,9 +1,21 @@
 import { Button, ButtonProps } from "@mui/material";
 import React from "react";
+import { theme } from "../theme";
 
 export function AppButton(props: React.PropsWithChildren<ButtonProps>) {
+  const bgColor =
+    props.variant === "outlined" ? "transparent" : theme.palette.primary.main;
   return (
-    <Button size="large" {...props}>
+    <Button
+      size="large"
+      {...props}
+      sx={{
+        ...props.sx,
+        "&:focus": {
+          backgroundColor: `${bgColor}!important`,
+        },
+      }}
+    >
       {props.children}
     </Button>
   );
